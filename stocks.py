6e7c32172313f74dfe2679105b4cc2ae8e21f896
Siberia <123393863+Dimitrii1993@users.noreply.github.com>
2025-11-23 23:08:00 +0300

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -692,3 +692,9 @@ chart.set_xlabel('Дата', fontdict={'family': 'Times New Roman', 'size': 10, 
 chart.set_yticks(range(150,400,30))
 
 chart.grid(True, axis = 'y', linestyle = '--', linewidth = 1, alpha = 0.2, color = 'black')
+
+chart.tick_params(
+    axis = 'y',
+    which = 'major',
+    labelsize = 7,
+    rotation = 10)
