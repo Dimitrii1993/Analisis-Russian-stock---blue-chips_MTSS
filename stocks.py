77c11d640ef6416730a025020331752de45b0654
Siberia <123393863+Dimitrii1993@users.noreply.github.com>
2025-11-23 23:08:16 +0300

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -698,3 +698,17 @@ chart.tick_params(
     which = 'major',
     labelsize = 7,
     rotation = 10)
+
+chart.tick_params(
+    axis = 'x',
+    which = 'major',
+    labelsize = 7,
+    rotation = 10)
+
+chart.xaxis.set_major_locator(mdates.MonthLocator())
+chart.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+
+chart.set_facecolor('yellow')
+
+plt.savefig('mtss_2025_volume.png', dpi = 300)
+
