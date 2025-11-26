67dd146a0d814383271ddfe80406d57340edce82
Siberia <123393863+Dimitrii1993@users.noreply.github.com>
2025-11-26 22:27:15 +0300

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -810,3 +810,9 @@ chart.set_facecolor('yellow')
 
 plt.savefig('mtss_2022_volume.png', dpi = 300)
 
+fig, chart = plt.subplots(nrows = 1, ncols = 1, figsize = (15,6))
+
+mask_1 = mtss_2025['date'] > '2023-01-01'
+mask_2 = mtss_2025['date'] < '2023-12-31'
+
+
