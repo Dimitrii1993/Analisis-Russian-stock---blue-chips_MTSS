e56cba8486d9c424a3c9cc85b1ec251df6c1c059
Siberia <123393863+Dimitrii1993@users.noreply.github.com>
2025-11-27 21:46:15 +0300

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -879,3 +879,9 @@ chart.plot(
     alpha = 0.5,
     color = 'black'
 )
+
+chart.legend()
+
+chart.set_title(f'Цена акции {name} за 2024г.', fontdict={'family': 'Times New Roman', 'size': 12, 'weight': 'bold'})
+chart.set_ylabel('Цена', fontdict = {'family': 'Times New Roman', 'size': 10, 'weight': 'normal'})
+chart.set_xlabel('Дата', fontdict={'family': 'Times New Roman', 'size': 10, 'weight': 'normal'})
