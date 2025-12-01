6b289f10f20f605517b33ba1351c13036217df6d
Siberia <123393863+Dimitrii1993@users.noreply.github.com>
2025-12-01 23:20:27 +0300

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -1084,3 +1084,12 @@ chart.legend()
 chart.set_title(f'Акции {name} за 2021 год ', fontdict = {'family': 'Times New Roman', 'size': 12, 'weight': 'bold'})
 chart.set_ylabel('Цена', fontdict = {'family': 'Times New Roman', 'size': 10, 'weight': 'normal'})
 chart.set_xlabel('Дата', fontdict = {'family': 'Times New Roman', 'size': 10, 'weight': 'normal'})
+
+chart.grid(True, axis = 'y', linestyle = '--', linewidth = 1, alpha = 0.2, color = 'black')
+
+chart.tick_params(
+    axis = 'y', 
+    which = 'major',
+    labelsize = 7,
+    rotation = 10
+)
