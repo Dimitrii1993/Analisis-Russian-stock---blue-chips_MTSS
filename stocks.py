75f05ffbf97e3b550dce79f4cb61ce8c4e5c7f1c
Siberia <123393863+Dimitrii1993@users.noreply.github.com>
2025-12-02 23:03:51 +0300

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -1106,3 +1106,10 @@ chart.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
 chart.set_facecolor('green')
 
 plt.savefig('mtss_2021.png', dpi = 300)
+
+fig, chart = plt. subplots(nrows = 1, ncols = 1, figsize = (15,6))
+
+mask_1 = mtss_2025['date'] > '2022-01-01'
+mask_2 = mtss_2025['date'] < '2022-12-30'
+
+
