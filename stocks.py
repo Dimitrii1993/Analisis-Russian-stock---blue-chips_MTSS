e0697daca6f7cbafeb6284e05f8fccaf662f09c0
Siberia <123393863+Dimitrii1993@users.noreply.github.com>
2025-12-06 13:21:04 +0300

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -1286,3 +1286,7 @@ chart.plot(
 )
 
 chart.legend()
+
+chart.set_title(f'Акции {name} за 2025 год ', fontdict = {'family': 'Times New Roman', 'size': 12, 'weight': 'bold'})
+chart.set_ylabel('Цена', fontdict = {'family': 'Times New Roman', 'size': 10, 'weight': 'normal'})
+chart.set_xlabel('Дата', fontdict = {'family': 'Times New Roman', 'size': 10, 'weight': 'normal'})
