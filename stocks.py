1488028f47cbbee172e4f83be323fb0f4a31980f
Siberia <123393863+Dimitrii1993@users.noreply.github.com>
2025-12-06 13:20:42 +0300

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -1275,3 +1275,14 @@ chart.scatter(
     marker = 'o',
     s = 10,
     c = 'black')
+
+chart.plot(
+    mtss_25_plot['date'],
+    mtss_25_plot['price_open'],
+    label = 'MTS_2025',
+    linewidth = 1,
+    linestyle = '--',
+    color = 'black'
+)
+
+chart.legend()
